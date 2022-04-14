976186586fd7e0f30111ac45327e97302087408e
David Paul Graham <dpgraham4401@gmail.com>
2022-04-14 13:27:49 -0400

    methods that use encode_manifest function can now accept a string instead of a json file.

--- emanifest-py/src/example.py
+++ emanifest-py/src/example.py
@@ -14,7 +14,7 @@
 #   json: string                  part of multipart/mixed response if applicable
 #   zip:  zipfile.ZipFile         part of multipart/mixed response if applicable
 # }
-
+import json
 import os
 from emanifest import client as em
 
@@ -29,19 +29,33 @@ def main():
 
     dot_numbers = rcra_client.GetManMethodCodes()
     # The Response can be accessed by calling the request.Response.json() method, or the json attribute for ease
+    # These should print the same json
     print(dot_numbers.response.json())
     print(dot_numbers.json)
 
     # Get Manifest json
     manifest = rcra_client.GetManByMTN(mtn)
-    with open('manifest.json', 'wb') as file:
-        file.write(manifest.response.content)
+    print(manifest.json)
+    # uncommenting the below will save manifest to './manifest.json'
+    # if manifest.ok:
+    #     with open('manifest.json', 'wb') as file:
+    #         file.write(manifest.response.content)
 
-    manifest_response = rcra_client.GetAttachments(mtn)
-    if manifest_response.ok:
-        # uncommenting the below line will save a number of files to your working directory
-        # manifest_response.zip.extractall()
-        print(manifest_response.json)
+    manifest_attachments = rcra_client.GetAttachments(mtn)
+    print(manifest_attachments.ok)
+    # uncommenting the below line will save a number of files to your working directory
+    # if manifest_attachments.ok:
+    #     manifest_attachments.zip.extractall()
+
+    # update the paper manifest with the path to .json and .zip file
+    update_resp = rcra_client.Update('example_update.json', 'example_update.zip')
+    print(update_resp.ok)
+
+    # or pass json as a string
+    with open('example_update.json') as f:
+        data = f.read() # data is a string containing the manifest json
+        update_resp = rcra_client.Update(data)
+        print(update_resp.ok)
 
 
 if __name__ == '__main__':
