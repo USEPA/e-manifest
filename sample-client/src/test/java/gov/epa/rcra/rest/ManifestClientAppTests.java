1fe91920c4bc879fb0fa79a4d3f67544ce286d38
David Paul Graham <dpgraham4401@gmail.com>
2024-04-10 08:15:11 -0400

    spring boot initializer

---
+++ sample-client/src/test/java/gov/epa/rcra/rest/ManifestClientAppTests.java
@@ -0,0 +1,13 @@
+package gov.epa.rcra.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManifestClientAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
