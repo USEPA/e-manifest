7bb77a278b6980896c58cb49d6c892a786425dd7
David Paul Graham <dpgraham4401@gmail.com>
2024-04-12 13:48:49 -0400

    Add lookup package which contains controllers and a client for fetching a returning results from the RCRAInfo lookup services such as the following     form code     density UOM     state waste code     federal waste codes     source codes     management method codes     waste minimization
    refactor class names for clarity and constrain public access to package internals

--- sample-client/src/main/java/gov/epa/rcra/rest/auth/AuthClient.java
+++ sample-client/src/main/java/gov/epa/rcra/rest/auth/AuthClient.java
@@ -1,6 +1,6 @@
 package gov.epa.rcra.rest.auth;
 
-import gov.epa.rcra.rest.client.RcraClient;
+import gov.epa.rcra.rest.client.RcraClientConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
@@ -17,8 +17,8 @@ public class AuthClient {
     @Value("${rcrainfo.api-id}")
     private String apiId;
 
-    AuthClient(RcraClient rcraClient) {
-        this.client = rcraClient.getClient();
+    AuthClient(RcraClientConfiguration rcraClientConfiguration) {
+        this.client = rcraClientConfiguration.getClient();
     }
 
     public RestClient authenticate() {
@@ -30,7 +30,6 @@ public class AuthClient {
         if (data == null) {
             throw new RuntimeException("Failed to authenticate");
         }
-        System.out.println("token: " + data.token());
         return client.mutate()
                 .defaultHeader("Authorization", "Bearer " + data.token()).build();
     }
--- sample-client/src/main/java/gov/epa/rcra/rest/lookup/LookupService.java
+++ sample-client/src/main/java/gov/epa/rcra/rest/lookup/LookupService.java
@@ -8,8 +8,13 @@ import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
+/**
+ * Service class to interact with the RCRAInfo lookup service
+ * ToDo: reduce the boilerplate, look at Spring HttpInterface + RestClient combo
+ * ToDo: refactor our into a LookupClient (for Http calls) and LookupService (for business logic)
+ */
 @Component
-public class LookupService {
+class LookupService {
 
     private final RestClient rcraClient;
 
@@ -18,11 +23,69 @@ public class LookupService {
         rcraClient = authClient.authenticate();
     }
 
-    public List<ContainerType> getContainerTypes() {
+    public List<RcraDescriptiveCode> getContainerTypes() {
         return rcraClient.get()
                 .uri("api/v1/emanifest/lookup/container-types")
                 .retrieve()
-                .body(new ParameterizedTypeReference<List<ContainerType>>() {
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+    }
+
+    public List<RcraDescriptiveCode> getWasteMinimizationCodes() {
+        return rcraClient.get()
+                .uri("api/v1/lookup/waste-minimization-codes")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+    }
+
+    public List<RcraDescriptiveCode> getFederalWasteCodes() {
+        return rcraClient.get()
+                .uri("api/v1/lookup/federal-waste-codes")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+
+    }
+
+    public List<RcraDescriptiveCode> getManagementMethodCodes() {
+        return rcraClient.get()
+                .uri("api/v1/lookup/management-method-codes")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+    }
+
+    public List<RcraDescriptiveCode> getDensityUOM() {
+        return rcraClient.get()
+                .uri("api/v1/lookup/density-uom")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+    }
+
+
+    public List<RcraDescriptiveCode> getFormCodes() {
+        return rcraClient.get()
+                .uri("api/v1/lookup/form-codes")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+    }
+
+    public List<RcraDescriptiveCode> getSourceCodes() {
+        return rcraClient.get()
+                .uri("api/v1/lookup/source-codes")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
+                });
+    }
+
+    public List<RcraDescriptiveCode> getStateWasteCodes(String stateCode) {
+        return rcraClient.get()
+                .uri("api/v1/lookup/state-waste-codes/" + stateCode)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<RcraDescriptiveCode>>() {
                 });
     }
 }
